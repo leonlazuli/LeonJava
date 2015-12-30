8ddee0ac1c4364a7522fec8e22846a870564a050
Leon Lazuli <leonlazuli@gmail.com>
2015-12-30 21:21:28 +0800

    add some comment for understanding of atomic and critical section

--- src/multithread/MySpinLock.java
+++ src/multithread/MySpinLock.java
@@ -4,7 +4,13 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Created by leonlazuli on 2015-12-30.
- * 这个程序加
+ * 这个加深了对 原子性 操作,和 critical section区别的理解. 原子性操作是保证操作不被打断,
+ * 而critical section的保护也是通过加锁的方式来保证了其执行不会被打断,但是加互斥锁会导致其他尝试
+ * 进入critical section的线程被阻塞(成为阻塞态), 而Atomic操作则是通过更底层的方式来保证这个cpu指令
+ * 执行不会被打断. 所以Atomic不涉及将其他进程阻塞,这样效率更高.
+ *
+ * 这里SpinLock的用处是,在等待时间比较少,并且线程数量少于处理器数量的时候,用spinlock可以避免转换线程状态的开销
+ * 从而提高效率.  但是如果等待时间长(多于转换线程状态的开销), 或者线程比处理器数量多,那么spinlock是不如用互斥锁.
  */
 public class MySpinLock {
     private AtomicBoolean locked = new AtomicBoolean();
